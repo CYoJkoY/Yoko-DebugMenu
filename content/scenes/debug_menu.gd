c95d862f5aa5d3cd149b16f76f2d065c171277a4
CYoJkoY <Cjy3430525946@outlook.com>
2026-04-20 10:11:03 +0800

    1011

--- content/scenes/debug_menu.gd
+++ content/scenes/debug_menu.gd
@@ -1,1 +1,22 @@
 extends "res://ui/menus/debug/debug_menu.gd"
+
+onready var cross_button: Button = $"%CrossButton"
+
+var focus_before_created: Control = null
+
+# =========================== Extension =========================== #
+func _on_cross_button_pressed() -> void:
+	if is_instance_valid(focus_before_created): focus_before_created.grab_focus()
+	queue_free()
+
+func _ready() -> void:
+	focus_before_created = get_focus_owner()
+	cross_button.grab_focus()
+
+	var enemies: Array = []
+	for entity in ItemService.entities:
+		if !(entity is ItemEnemy): continue
+
+		enemies.append(entity)
+
+	inventory_enemies._elements.set_elements(enemies)
--- extensions/debug_service.gd
+++ extensions/debug_service.gd
@@ -3,12 +3,15 @@ extends "res://singletons/debug_service.gd"
 var mod_debug_menu = null
 
 # =========================== Extension =========================== #
-func _input(event):
-    ._input(event)
-
-    if OS.is_debug_build() or !event.is_action_pressed("open_debug_menu"): return
+func _input(_event) -> void:
+    if (!Input.is_physical_key_pressed(KEY_T) and \
+            !(
+                _event.is_action_pressed("ltrigger") or \
+                _event.is_action_pressed("rtrigger")
+              )
+       ) or is_instance_valid(mod_debug_menu): return
     
-    if is_instance_valid(mod_debug_menu): return
+    get_viewport().set_input_as_handled()
 
     mod_debug_menu = load("res://mods-unpacked/Yoko-DebugMenu/content/scenes/debug_menu.tscn").instance()
     if get_tree().current_scene is Main: get_tree().current_scene.get_node("UI").add_child(mod_debug_menu)
